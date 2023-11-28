df1ddbf235b730b5d15b76f91ff1bbdc5775e59f
lizziejuks <91128262+lizziejuks@users.noreply.github.com>
2023-11-28 14:08:10 +0300

    Add files via upload

---
+++ loan.py
@@ -0,0 +1,7 @@
+#program to calculate loan
+income=float(input("enter income"))
+age=int(input("enter age"))
+if income >= 21000 and age>=18:
+    print("congrats you qualify for a loan")
+ else:
+     print("you do not qualify for a loan")
