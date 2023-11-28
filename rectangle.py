1178110b1721ca4485710ecb8e2dc38d0c2e91cd
lizziejuks <91128262+lizziejuks@users.noreply.github.com>
2023-11-28 13:39:58 +0300

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+#creating a class and object
+class Rectangle:
+    def _init_(self,length,width):
+        self.length=length
+        self.width=width
+#calculate area
+def area(self):
+    a=self.length*self.width
+    return area
+#calculate perimeter
+def perimeter(self):
+    p=2*(self.length+self.width)
+    return p
+#display output
+rec=Rectangle(4,8<)
+print(rec.perimeter())
+print(rec.area())
