50fd8e4041d00b6cad59979415929170c3da76e2
lizziejuks <91128262+lizziejuks@users.noreply.github.com>
2023-11-28 14:22:05 +0300

    Add files via upload

---
+++ list as stacks.py
@@ -0,0 +1,10 @@
+#lists as stacks
+    stack = ['a','b','c','d']
+    stack.append('e')
+    stack.append('f')
+    stack
+['a', 'b', 'c', 'd', 'e', 'f']
+    stack.pop()
+'f'
+    stack
+['a, 'b', 'c', 'd', 'e']
